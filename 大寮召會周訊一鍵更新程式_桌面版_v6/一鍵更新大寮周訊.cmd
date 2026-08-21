4ef912a6fd567cb56a6011f8d6b87ecbb26f6bd7
吳加恩 <b112254121@nkust.edu.tw>
2026-08-21 15:07:05 +0800

    Delete 大寮召會周訊一鍵更新程式_桌面版_v6/一鍵更新大寮周訊.cmd

--- 大寮召會周訊一鍵更新程式_桌面版_v6/一鍵更新大寮周訊.cmd
+++
@@ -1,7 +0,0 @@
-@echo off
-chcp 65001 >nul
-title Weekly Bulletin Updater 2026.08.15-v6
-echo Program folder: %~dp0
-powershell.exe -NoProfile -ExecutionPolicy Bypass -File "%~dp0updater.ps1"
-echo.
-pause
